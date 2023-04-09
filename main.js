5e5704ab0898cffe65b0f543cf8bcee7623cfd8a
Reevo13 <121067601+Reevo13@users.noreply.github.com>
2023-04-09 13:05:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const body = document.body
+
+
+body.style.background = ''
+
+function ab() {
+  location.href = '#abl'
+}
+
+function tp() {
+  gg.style.display = 'none'
+}
+
+function sl() {
+  gg.style.display = 'inline'
+}
+
+function skk() {
+  location.href = '#skl'
+  
+}
+
+function hh() {
+  location.href = '#'
+  
+}
+
+function by() {
+  location.href = 'https://wa.me/6285175031301?text=Halo%20Mas%20Saya%20Mau%20Order'
+  
+}
+
+function ig() {
+  location.href = '#https://www.instagram.com/reevo888/'
+  
+}
